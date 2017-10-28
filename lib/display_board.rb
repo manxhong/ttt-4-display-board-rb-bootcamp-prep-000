70d6c4d7f9fa339a3a8e86a30da1d021dc1a7ebc
Man Hong Lee <manhong323@gmail.com>
2017-10-28 21:25:21 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-var cells = "   |   |   "
-var rows = "-----------";
+cells = "   |   |   "
+rows = "-----------";
 def display_board(board)
   puts "#{cells}/n#{rows}/n#{cells}/n#{rows}/n#{cells}"
 end
