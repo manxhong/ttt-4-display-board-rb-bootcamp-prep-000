28d69b573879fa8698263f7e741a5104cc0e3e56
Man Hong Lee <manhong323@gmail.com>
2017-10-28 21:37:11 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -6,6 +6,6 @@ def display_board(board = "")
   puts " #{board[0]=" "} | #{board[1]=" "} | #{board[2]=" "} "+
 "\n#{rows}
  #{board[3]=" "} | #{board[4]=" "} | #{board[5]=" "} "+
-"#{rows}
+"\n#{rows}
  #{board[6]=" "} | #{board[7]=" "} | #{board[8]=" "} "
 end
