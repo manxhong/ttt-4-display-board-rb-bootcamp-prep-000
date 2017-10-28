32a7413d0038bce51127ee58e15d8fa3f688a642
Man Hong Lee <manhong323@gmail.com>
2017-10-28 21:23:53 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,7 +1,7 @@
 # Define display_board that accepts a board and prints
 # out the current state.
-var cells = "   "
-var rows = "-----------"l
+var cells = "   |   |   "
+var rows = "-----------";
 def display_board(board)
-  
+
 end
